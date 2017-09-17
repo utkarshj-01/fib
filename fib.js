99d25e1ec9823ee95402a64a48ab55db6fead935
utkarsh-01 <utkarshjaiswal987@gmail.com>
2017-09-17 16:39:57 +0530

    Fibonacci No. generate

--- fib.js
+++ fib.js
@@ -1,11 +1,21 @@
-var i,p,n,c=0;
-p=0;
-n=1;
-console.log(n);
-while(c<31)
+function fibo(){
+var a=new Array();
+a[0]=0;
+a[1]=1;
+var i=2;
+while(1)
 {
-c=p+n;
-p=n;
-n=c;
-console.log(c);
+
+a[i]=a[i-1]+a[i-2];
+i++;
+if(a[i-1]+a[i-2]>30)
+break;
 }
+return(a);
+}
+
+var c=new Array(fibo());
+var n=c.length;
+
+console.log(c);
+module.exports=fibo;
